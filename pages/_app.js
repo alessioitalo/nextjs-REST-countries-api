32858f8d15d5967f49cae012c21ba7bfa8a2339b
Alessio Italo Junior D'Alise <Alessioitalo@gmail.com>
2021-10-19 13:37:25 +0100

    moved context from index to app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { ThemeProvider } from 'styled-components';
+import { useState } from 'react';
+import { lightTheme, darkTheme, CountriesContext } from '../store';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [currentTheme, setCurrentTheme] = useState('dark');
+  const [filteredCountries, setFilteredCountries] = useState([]);
+  const [countries, setCountries] = useState([])
+  return (
+    <CountriesContext.Provider
+      value={{
+        currentTheme,
+        setCurrentTheme,
+        filteredCountries,
+        setFilteredCountries,
+        countries,
+        setCountries,
+      }}
+    >
+      <ThemeProvider theme={currentTheme === 'dark' ? darkTheme : lightTheme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CountriesContext.Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,57 +1,35 @@
-import Head from 'next/head';
 import Layout from '../components/Layout';
 import Refine from '../components/Refine';
-import { ThemeProvider } from 'styled-components';
-import { useState } from 'react';
-import { lightTheme, darkTheme, CountriesContext } from '../store';
+import { CountriesContext } from '../store';
 import MiniCard from '../components/MiniCard';
-import { useRouter } from 'next/router';
+// import { useRouter } from 'next/router';
+import { useContext, useEffect } from 'react';
 
 export async function getStaticProps() {
   const response = await fetch('https://restcountries.com/v3.1/all');
   const countries = await response.json();
-  // const countries = Object.keys(data).map(key=>{
-  //   return data[key]
-  // })
-
   return {
     props: { countries: countries },
   };
 }
 
 export default function Home({ countries }) {
-  const [currentTheme, setCurrentTheme] = useState('dark');
-  const [filteredCountries, setFilteredCountries] = useState(countries);
-  const router = useRouter();
-  return (
-    <CountriesContext.Provider
-      value={{
-        currentTheme,
-        setCurrentTheme,
-        filteredCountries,
-        setFilteredCountries,
-        countries,
-      }}
-    >
-      <ThemeProvider theme={currentTheme === 'dark' ? darkTheme : lightTheme}>
-        <Layout>
-          <Refine />
-          <div className='container'>
-            {/* country here... */}
-            {/* <p>{(countries[0].name.common)}</p> */}
-            {filteredCountries.slice(0, 20).map((country) => (
-              <MiniCard key={country.cca2} country={country} />
-            ))}
-          </div>
-        </Layout>
-      </ThemeProvider>
-    </CountriesContext.Provider>
-  );
-}
+  useEffect(() => {
+    ctx.setCountries(countries);
+    ctx.setFilteredCountries(countries);
+  }, []);
 
-{
-  /* <ul key={country.altSpellings}>
-                <li>{country.name.common}</li>
-                <li>{country.population}</li>
-              </ul> */
+  const ctx = useContext(CountriesContext);
+  // const router = useRouter();
+  return (
+    <Layout>
+      <Refine />
+      <div className='container'>
+        {ctx.filteredCountries.slice(0, 20).map((country) => (
+          <MiniCard key={country.cca2} country={country} />
+        ))}
+      </div>
+    </Layout>
+
+  );
 }
